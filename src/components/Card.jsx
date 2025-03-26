e0c94de023c4cdf8e8d609cc8df60f602b1a9d2f
Prince Manzi <manziprincemp@gmail.com>
2025-03-26 20:16:15 +0200

    initial commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
-const Card = ({children}) => {
+const Card = ({children, bg = 'bg-gray-100'}) => {
   return (
-    <div className='bg-gray-100 p-6 rounded-lg shadow-md'> { children } </div>
+    <div className={`${bg} p-6 rounded-lg shadow-md`}> { children } </div>
   )
 }
 
