8c072ae116b98193ef0d01dcf6ebbd97a0be1b23
Prince Manzi <manziprincemp@gmail.com>
2025-03-27 21:49:36 +0200

    initial commit

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -2,6 +2,7 @@ import Jobs from "../jobs.json";
 import JobListing from "./JobListing";
 
 const JobListings = () => {
+  const recentJobs = Jobs.slice(0, 3);
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
@@ -9,7 +10,7 @@ const JobListings = () => {
           Browse Jobs
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {Jobs.map((job) => (
+          {recentJobs.map((job) => (
             <JobListing key={job.id} job={job} />
           ))}
         </div>
