d67b0c0be1a1bb3f998709baab796832697fa751
Prince Manzi <manziprincemp@gmail.com>
2025-03-29 18:46:47 +0200

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ export default {
                 sans: ['Roboto', 'sans-serif'],
             },
             gridTemplateColumns: {
-                '70/30': '70% 28%',
+                '70/30': '70% 30%',
             },
         },
 
