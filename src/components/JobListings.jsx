bd89ee1b9d9f00c84c01731d007b915c42fd85fb
Prince Manzi <manziprincemp@gmail.com>
2025-03-29 12:26:07 +0200

    initial commit

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,16 +1,16 @@
 import Jobs from "../jobs.json";
 import JobListing from "./JobListing";
 
-const JobListings = () => {
-  const recentJobs = Jobs.slice(0, 3);
+const JobListings = ({ isHome }) => {
+  const jobListings = isHome ? Jobs.slice(0, 3) : Jobs; // Show only 3 jobs on home page
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-          Browse Jobs
+          {isHome ? "Recent Jobs" : "Browse All Jobs"}
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {recentJobs.map((job) => (
+          {jobListings.map((job) => (
             <JobListing key={job.id} job={job} />
           ))}
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,54 +1,42 @@
-import { Link } from 'react-router-dom'  // importing link from react-router-dom prevents actual refresh of the page by changing a tags to link tags and href to to
+import { NavLink } from "react-router-dom"; // importing link from react-router-dom prevents actual refresh of the page by changing a tags to link tags and href to to
 
-import logo from '../assets/images/logo.png'
+import logo from "../assets/images/logo.png";
 
 const Navbar = () => {
+  const className = ({ isActive }) =>
+    isActive
+      ? "text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+      : "text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2";
   return (
     <nav className="bg-indigo-700 border-b border-indigo-500">
-        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-          <div className="flex h-20 items-center justify-between">
-            <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
-              {/* <!-- Logo --> */}
-              <Link
-                className="flex flex-shrink-0 items-center mr-4"
-                to="/"
-              >
-                <img
-                  className="h-10 w-auto"
-                  src={logo}
-                  alt="React Jobs"
-                />
-                <span className="hidden md:block text-white text-2xl font-bold ml-2">
-                  React Jobs
-                </span>
-              </Link>
-              <div className="md:ml-auto">
-                <div className="flex space-x-2">
-                  <Link
-                    to="/"
-                    className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >
-                    Home
-                  </Link>
-                  <Link
-                    to="/jobs"
-                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >
-                    Jobs
-                  </Link>
-                  <Link
-                    to="/add-job"
-                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >
-                    Add Job
-                  </Link>
-                </div>
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="flex h-20 items-center justify-between">
+          <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
+            {/* <!-- Logo --> */}
+            <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
+              <img className="h-10 w-auto" src={logo} alt="React Jobs" />
+              <span className="hidden md:block text-white text-2xl font-bold ml-2">
+                React Jobs
+              </span>
+            </NavLink>
+            <div className="md:ml-auto">
+              <div className="flex space-x-2">
+                <NavLink to="/" className={className}>
+                  Home
+                </NavLink>
+                <NavLink to="/jobs" className={className}>
+                  Jobs
+                </NavLink>
+                <NavLink to="/add-job" className={className}>
+                  Add Job
+                </NavLink>
               </div>
             </div>
           </div>
         </div>
-      </nav>
-  )
-}
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
